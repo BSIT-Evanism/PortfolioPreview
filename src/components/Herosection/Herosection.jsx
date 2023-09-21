8e6c822a21c6e7ec13b50d7e57fd607ff26d4504
BSIT-Evanism <71175442+BSIT-Evanism@users.noreply.github.com>
2023-09-22 02:50:09 +0800

    first comm

---
+++ src/components/Herosection/Herosection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Herosection.module.scss";
+
+function Herosection() {
+  return (
+    <div className={styles.wrapper}>
+      <h1 className={styles.title}>Herosection</h1>
+    </div>
+  );
+}
+
+export default Herosection;
