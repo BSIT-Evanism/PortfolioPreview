1f8b4ec65eab20fe84830caa885584afe7acf80b
BSIT-Evanism <71175442+BSIT-Evanism@users.noreply.github.com>
2023-09-22 17:33:20 +0800

    0.0.2

--- src/components/Herosection/Herosection.jsx
+++ src/components/Herosection/Herosection.jsx
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./Herosection.module.scss";
+import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 
 function Herosection() {
+  const { scrollYProgress } = useScroll();
+  const springY = useSpring(scrollYProgress);
+  const coords = useTransform(springY, [0, 1], [0, 200]);
+
   return (
-    <div className={styles.wrapper}>
-      <h1 className={styles.title}>Herosection</h1>
-    </div>
+    <>
+      <motion.div className={styles.scrollbar}>
+        <motion.div
+          style={{ y: coords }}
+          className={styles.scroller}
+        ></motion.div>
+      </motion.div>
+      <div className={styles.wrapper}>
+        <div className={styles.border}>
+          <div className={styles.top}>
+            <div id="box" className={styles.center}>
+              <h1>WORK IN PROGRESS...</h1>
+            </div>
+          </div>
+          <div className={styles.title}>
+            <h1 className="innerTitle">evan solanoy</h1>
+            <img
+              className="innerImage"
+              src="../../../public/evan.svg"
+              alt="evan"
+            />
+          </div>
+        </div>
+        <div className={styles.footer}>
+          <h2 id="inview">check out my other creations...</h2>
+        </div>
+      </div>
+    </>
   );
 }
 
