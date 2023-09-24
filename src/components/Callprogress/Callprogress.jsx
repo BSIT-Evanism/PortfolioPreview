267d9f5575aac4958e1bd789260831a0ff362470
BSIT-Evanism <71175442+BSIT-Evanism@users.noreply.github.com>
2023-09-24 14:52:09 +0800

    added more media and hover effects

--- src/components/Callprogress/Callprogress.jsx
+++ src/components/Callprogress/Callprogress.jsx
@@ -1,16 +1,34 @@
 import React from 'react'
 import styles from './Callprogress.module.scss';
+import { motion } from 'framer-motion';
 
 function Callprogress() {
+  const [toggle, setToggle] = React.useState(false)
+
   return (
     <div className={styles.wrapper}>
-        <div className={styles.button}>
-          <div className={styles.notice}>
-            <div className={styles.ping}></div>
-            <h1>Available for work</h1>
-          </div>
+      <div className={styles.button}>
+        <div 
+          className={styles.notice}
+          onMouseEnter={() => setToggle(true)}
+          onMouseLeave={() => setToggle(false)}
+          >
+          <div className={styles.ping}></div>
+          <h1>Available for work</h1>
+          <motion.div 
+            className={styles.callAction}
+            animate={{y: toggle ? 0 : 100}}
+            >
+            <h1>Contact me</h1>
+          </motion.div>
         </div>
-        <div className={styles.work}>Callprogress</div>
+      </div>
+      <div className={styles.work}>
+        <div className={styles.textwrap}>
+          <h1>WORK IN PROGRESS...</h1>
+          <h1>WORK IN PROGRESS...</h1>
+        </div>
+      </div>
     </div>
   )
 }
