ce36dd75829c59989ae5f707d150ab03d4b4f2c7
BSIT-Evanism <johnevanporcioncula.solanoy@bicol-u.edu.ph>
2023-10-01 22:29:06 +0800

    added and fixed width mq

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
 import styles from './Header.module.scss';
+import { useScroll, motion } from 'framer-motion';
 
 function Header() {
+  const { scrollYProgress } = useScroll()
+
+
   return (
     <div className={styles.wrapper}>
       <p>software</p>
-        <div className={styles.rollerwrapper}>
-          
-            <h1>evan solanoy</h1>
-            <img src='/evan.svg' alt='evan' />
-          
-        </div>
-        <p>engineer</p>
+      <div className={styles.rollerwrapper}>
+
+        <motion.h1
+          animate={{ opacity: scrollYProgress }}
+        >evan solanoy</motion.h1>
+        <img src='/evan.svg' alt='evan' />
+
+      </div>
+      <p>engineer</p>
     </div>
   )
 }
